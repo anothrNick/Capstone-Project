57165021418cce21e5d0eed1bf95a98c9fd4bb21
Nick Sjostrom <nick.sjostrom12@gmail.com>
2015-10-06 00:13:59 -0400

    Style to arrow buttons. Update textview value

--- app/src/main/java/com/dev/nick/scorch/games/GameDetailActivity.java
+++ app/src/main/java/com/dev/nick/scorch/games/GameDetailActivity.java
@@ -6,12 +6,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.dev.nick.scorch.R;
 
 public class GameDetailActivity extends AppCompatActivity {
 
     private Button finishGameBtn;
+
+    private ImageButton scoreOneUp;
+    private ImageButton scoreOneDown;
+    private ImageButton scoreTwoUp;
+    private ImageButton scoreTwoDown;
+
+    private TextView scoreOne;
+    private TextView scoreTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +30,55 @@ public class GameDetailActivity extends AppCompatActivity {
 
         finishGameBtn = (Button) findViewById(R.id.finishBtn);
 
+        scoreOne = (TextView) findViewById(R.id.team_one_score);
+        scoreTwo = (TextView) findViewById(R.id.team_two_score);
+
+        scoreOneDown = (ImageButton) findViewById(R.id.score_one_down);
+        scoreOneUp = (ImageButton) findViewById(R.id.score_one_up);
+        scoreTwoDown = (ImageButton) findViewById(R.id.score_two_down);
+        scoreTwoUp = (ImageButton) findViewById(R.id.score_two_up);
+
+        scoreOneDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int val = Integer.parseInt(scoreOne.getText().toString());
+                val--;
+                scoreOne.setText(Integer.toString(val));
+            }
+        });
+
+        scoreOneUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int val = Integer.parseInt(scoreOne.getText().toString());
+                val++;
+                scoreOne.setText(Integer.toString(val));
+            }
+        });
+
+        scoreTwoDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int val = Integer.parseInt(scoreTwo.getText().toString());
+                val--;
+                scoreTwo.setText(Integer.toString(val));
+            }
+        });
+
+        scoreTwoUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int val = Integer.parseInt(scoreTwo.getText().toString());
+                val++;
+                scoreTwo.setText(Integer.toString(val));
+            }
+        });
+
         finishGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // TODO
+                // complete game
                 finish();
             }
         });
